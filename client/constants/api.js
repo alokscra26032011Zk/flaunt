f146de9aede7694295eced866c92a5b520f1e149
aloscra26032011Zk <alokscra26032011@gmail.com>
2017-03-08 09:27:03 +0530

    init

---
+++ client/constants/api.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { Platform } from 'react-native';
+
+let url;
+
+// Cause of genymotion we need to change the url here
+// http://stackoverflow.com/questions/5528850/how-to-connect-localhost-in-android-emulator
+if (Platform.OS !== 'ios') {
+  url = 'http://10.0.3.2:3000/api';
+} else {
+  url = 'http://localhost:3000/api';
+}
+
+axios.defaults.baseURL = url;
+
+const fakeGroupId = '58ad10b7dbeef9575d00b376';
+
+class MeetupApi {
+  constructor() {
+    this.groupId = fakeGroupId;
+    this.path = `/groups/${this.groupId}/meetups`;
+  }
+
+  async fetchGroupMeetups() {
+    const { data } = await axios.get(this.path);
+
+    return data.meetups;
+  }
+}
+
+export {
+  MeetupApi
+};
---
+++ client/main.js
@@ -0,0 +1,48 @@
+import Exponent, { Components } from 'exponent';
+import React from 'react';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import Colors from './constants/Colors';
+import { cachedFonts } from './helpers';
+import Root from './src/Root';
+
+EStyleSheet.build(Colors);
+
+class App extends React.Component {
+  state = {
+    fontLoaded: false
+  }
+
+  componentDidMount() {
+    this._loadAssetsAsync();
+  }
+
+  async _loadAssetsAsync() {
+    const fontAssets = cachedFonts([
+      {
+        montserrat: require('./assets/fonts/Montserrat-Regular.ttf')
+      },
+      {
+        montserratBold: require('./assets/fonts/Montserrat-Bold.ttf')
+      },
+      {
+        montserratLight: require('./assets/fonts/Montserrat-Light.ttf')
+      },
+      {
+        montserratMed: require('./assets/fonts/Montserrat-Medium.ttf')
+      }
+    ]);
+
+    await Promise.all(fontAssets);
+
+    this.setState({ fontLoaded: true });
+  }
+
+  render() {
+    if (!this.state.fontLoaded) {
+      return <Components.AppLoading />;
+    }
+    return <Root />;
+  }
+}
+
+Exponent.registerRootComponent(App);
